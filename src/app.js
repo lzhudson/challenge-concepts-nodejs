8261012327e78ae8337b6b794d9df2f617a6ae0b
Hudson Holanda <hudsonholanda55@gmail.com>
2021-03-06 17:28:09 -0300

    feat: update likes in repository

--- src/app.js
+++ src/app.js
@@ -49,7 +49,12 @@ app.delete("/repositories/:id", (request, response) => {
 });
 
 app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+  const { id } = request.params;
+
+  const repositoryIndex = repositories.findIndex(repository => repository.id === id);
+  repositories[repositoryIndex].likes += 1;
+
+  return response.json(repositories[repositoryIndex]);
 });
 
 module.exports = app;
